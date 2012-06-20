d08b04a98e2717a1f22aa33e0867c1a46bef4759
mamoo <m.pedriale@gmail.com>
2012-06-21 00:01:47 +0200

    Privace scope introduction.

--- module.js
+++ module.js
@@ -1,16 +1,25 @@
-Module = Class.extend({
-	initialize: function(sandbox, options){
-		if (!sandbox || !(sandbox instanceof Sandbox)) throw new TypeError("Sandbox");
-		this.sandbox = sandbox;
-
-		if (options && options.subscribesTo) {
+Module = Class.extend((function(){
+	var _init = function(sandbox, options){
+			if (!sandbox || !(sandbox instanceof Sandbox)) throw new TypeError("Sandbox");
+			this.sandbox = sandbox;
+		},
+		_initCallbacks = function(sandbox, options, callbacks){
+			if (!options || !options.subscribesTo) {
+				return;
+			}
 			var i;
 			for (i = 0; i < options.subscribesTo.length; i++){
 				var message = options.subscribesTo[i];
-				if (typeof this.callbacks[message] === "undefined")
-					throw new ReferenceError();
-				sandbox.subscribe(message, this.callbacks[message]);
-			}
+				if (typeof callbacks[message] === "undefined"){
+					throw new ReferenceError();					
+				}
+				sandbox.subscribe(message, callbacks[message]);
+			}			
+		};
+	return {
+		initialize: function(sandbox, options){
+			_init(sandbox, options);
+			_initCallbacks(sandbox, options, this.callbacks || {});
 		}
 	}
-});
+})());
--- tests/spec/moduleSpec.js
+++ tests/spec/moduleSpec.js
@@ -3,79 +3,74 @@ describe("A Module", function() {
 
   });
 
-  it("can be instantiated", function() {
-    var module = new Module(new Sandbox(Core));
-    expect(module.sandbox instanceof Sandbox).toBe(true);
-  });
-
   it("rejects bad type sandboxes", function() {
-    var foo = function(){ 
-      new Module("sss"); 
-    };
-    expect(foo).toThrow();
+	var foo = function(){ 
+	  new Module("sss"); 
+	};
+	expect(foo).toThrow();
   });
 
   it("accepts no options", function(){
-    var foo = function(){ 
-      new Module(new Sandbox(Core)); 
-    };
-    expect(foo).not.toThrow();
+	var foo = function(){ 
+	  new Module(new Sandbox(Core)); 
+	};
+	expect(foo).not.toThrow();
   });
 
   it("checks for callback presence when list of events to be subscribed is passed through options", function(){
-    var foo = function(){ 
-      new Module(
-        new Sandbox(Core), 
-        { subscribesTo: ["a","b","c"] }
-      ); 
-    };
-    expect(foo).toThrow();
+	var foo = function(){ 
+	  new Module(
+		new Sandbox(Core), 
+		{ subscribesTo: ["a","b","c"] }
+	  ); 
+	};
+	expect(foo).toThrow();
   });
 
   it("automatically subscribes to a list of events if passed through options", function(){
-    var MyModule = Class.extend(Module, {
-      initialize:function(sandbox, options){
-        this.parent(sandbox, options);
-      },
-      callbacks : {
-        a: function(){
+	var MyModule = Class.extend(Module, {
+	  	initialize:function(sandbox, options){
+			this.parent(sandbox, options);
+	  	},
+	  	callbacks:{
+				a: function(){
 
-        },
-        b: function(){
+				},
+				b: function(){
 
-        },
-        c: function(){
+				},
+				c: function(){
 
-        }
-      }
-    });
+				}
+		}
+	});
 
-    var foo = function(){ 
-      new MyModule(
-        new Sandbox(Core), 
-        { subscribesTo: ["a","b","c"] }
-      ); 
-    };
-    expect(foo).not.toThrow();
+	var foo = function(){ 
+	  new MyModule(
+		new Sandbox(Core), 
+		{ subscribesTo: ["a","b","c"] }
+	  ); 
+	};
+	expect(foo).not.toThrow();
   });  
 
   it("is able to receive a subscribed message", function(){
-    var i = 0;
-    var MyModule = Class.extend(Module, {
-      initialize:function(sandbox, options){
-        this.parent(sandbox, options);
-      },
-      callbacks: {
-        a : function(){
-          i++;
-        }
-      }
-    });
-    var myModule = new MyModule(
-        new Sandbox(Core), 
-        { subscribesTo: ["a"] }
-      ); 
-    Core.publish("a");
-    expect(i).toBe(1);
+	var i = 0;
+	var MyModule = Class.extend(Module, {
+	  initialize:function(sandbox, options){
+		this.parent(sandbox, options);
+	  },
+	  callbacks: {
+		a : function(){
+		  i++;
+		}
+	  }
+	});
+	var myModule = new MyModule(
+		new Sandbox(Core), 
+		{ subscribesTo: ["a"] }
+	  ); 
+	Core.publish("a");
+	expect(i).toBe(1);
   });    
 });
